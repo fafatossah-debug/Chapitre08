90d833c9d5166b3a84fbcb214d94cbc6d842352d
Tossah Fafa <fafatossah@gmail.com>
2026-05-28 11:03:05 +0000

    methode add() refectorisée

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -13,4 +13,13 @@ public class BookShelfSpec {
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
+
+@Test
+void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+    BookShelf shelf = new BookShelf();
+    shelf.add("Effective Java");
+    shelf.add("Code Complete");
+    List<String> books = shelf.books();
+    assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+}
 }
