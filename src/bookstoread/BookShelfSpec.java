7ecd19a69c90b1c6a5a17ae0ceeaef2bac3f4700
Tossah Fafa <fafatossah@gmail.com>
2026-05-28 11:16:31 +0000

    methode ajouté  emptyBookShelfWhenAddIsCalledWithoutBooks

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,5 +1,6 @@
 package bookstoread;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -14,12 +15,19 @@ public class BookShelfSpec {
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
 
-@Test
-void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
-    BookShelf shelf = new BookShelf();
-    shelf.add("Effective Java");
-    shelf.add("Code Complete");
-    List<String> books = shelf.books();
-    assertEquals(2, books.size(), () -> "BookShelf should have two books.");
-}
+    @Test
+    void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java", "Code Complete");
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+    }
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+        BookShelf shelf = new BookShelf();
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+
 }
