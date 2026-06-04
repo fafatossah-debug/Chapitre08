bbcf631d34910fdaf177b25d3b152b4c6397bb66
Tossah Fafa <fafatossah@gmail.com>
2026-06-04 09:53:38 +0000

    Ajout de bookshelfArrangedByBookTitle pour vérifier l'ajout de livre à la liste

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -3,12 +3,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BookShelfSpec {
+public class BookShelfSpec{
     private BookShelf shelf;
 
     @BeforeEach
@@ -46,5 +47,11 @@ public class BookShelfSpec {
         } catch (Exception e) {
             assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException.");
         }
+    }
+    @Test
+    void bookshelfArrangedByBookTitle() {
+        shelf.add("Effective Java", "Code Complete","The Mythical Man-Month" );
+        List<String> books = shelf.arrange();
+        assertEquals(Arrays.asList("Code Complete", "Effective Java", "The Mythical Man-Month"), books, () -> "Books in a bookshelf should be arranged lexicographically by book title");
     }
 }
